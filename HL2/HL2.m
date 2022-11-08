70e49890c6e3f95ba321a64a27b26dd1da56a994
Fonzius <posta.marcus99@gmail.com>
2022-11-08 13:27:53 +0100

    Now with ex2.a

--- HL2/HL2.m
+++ HL2/HL2.m
@@ -16,11 +16,23 @@ set_param('HL2/C', 'C', num2str(C));
 set_param('HL2/L', 'L', num2str(L));
 
 alpha = R/(2*L);
-%%
+%% Ex 1.b
 f = 0:Fs/length(out.impulse.Data):Fs-(1/length(out.impulse.Data));
 H = (abs(fft(out.impulse_response.Data)./(fft(out.impulse.Data))));
  
 figure()
 plot(f,H);
+xlim([0,Fs/2]);
 
 f_0 = c/(2*pi) * sqrt(S/(V*l));
+
+%% Ex 2.a
+set_param('HL2_ex2/R', 'R', num2str(R));
+set_param('HL2_ex2/C', 'C', num2str(C));
+set_param('HL2_ex2/L', 'L', num2str(L));
+
+H_branch = (abs(fft(out.output.Data)./(fft(out.input.Data))));
+ 
+figure()
+plot(f,H_branch);
+xlim([0,Fs/2]);

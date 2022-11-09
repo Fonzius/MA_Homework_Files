3203f589935031e433746c0876cc35bc31573abb
Fonzius <posta.marcus99@gmail.com>
2022-11-09 17:12:57 +0100

    Happy coding time for all the family

--- HL2/HL2.m
+++ HL2/HL2.m
@@ -1,3 +1,7 @@
+clc;
+clear all;
+close all;
+
 %% Ex 1.a
 V = 0.1; %[m^3]
 l = 0.1; %[m]
@@ -11,11 +15,13 @@ R = rho*c/S;
 L = rho*l/S;
 C = V/(rho*c^2);
 
-set_param('HL2/R', 'R', num2str(R));
-set_param('HL2/C', 'C', num2str(C));
-set_param('HL2/L', 'L', num2str(L));
+set_param('HL2_ex1', 'PreLoadFcn', num2str(Fs));
+set_param('HL2_ex1/Resonator/R', 'R', num2str(R));
+set_param('HL2_ex1/Resonator/C', 'C', num2str(C));
+set_param('HL2_ex1/Resonator/L', 'L', num2str(L));
 
-alpha = R/(2*L);
+out = sim('HL2_ex1', dur);
+
 %% Ex 1.b
 f = 0:Fs/length(out.impulse.Data):Fs-(1/length(out.impulse.Data));
 H = (abs(fft(out.impulse_response.Data)./(fft(out.impulse.Data))));
@@ -27,12 +33,39 @@ xlim([0,Fs/2]);
 f_0 = c/(2*pi) * sqrt(S/(V*l));
 
 %% Ex 2.a
-set_param('HL2_ex2/R', 'R', num2str(R));
-set_param('HL2_ex2/C', 'C', num2str(C));
-set_param('HL2_ex2/L', 'L', num2str(L));
 
-H_branch = (abs(fft(out.output.Data)./(fft(out.input.Data))));
- 
+subsystems = find_system('HL2_ex2a','SearchDepth',2,'BlockType','SubSystem');
+
+for ii = 1:length(subsystems)
+    if contains(subsystems(ii), 'Resonator')
+        set_param(char(append(subsystems(ii),'/R')), 'R', num2str(R));
+        set_param(char(append(subsystems(ii),'/C')), 'C', num2str(C));
+        set_param(char(append(subsystems(ii),'/L')), 'L', num2str(L));
+    end
+end
+set_param('HL2_ex2a', 'PreLoadFcn', num2str(Fs));
+%%
+H_2a = (abs(fft(out.output.Data)./(fft(out.input.Data))));
+
 figure()
-plot(f,H_branch);
+plot(f,H_2a);
 xlim([0,Fs/2]);
+title("2 branch");
+%% Ex 2.b
+subsystems = find_system('HL2_ex2b','SearchDepth',2,'BlockType','SubSystem');
+
+for ii = 1:length(subsystems)
+    if contains(subsystems(ii), 'Resonator')
+        set_param(char(append(subsystems(ii),'/R')), 'R', num2str(R));
+        set_param(char(append(subsystems(ii),'/C')), 'C', num2str(C));
+        set_param(char(append(subsystems(ii),'/L')), 'L', num2str(L));
+    end
+end
+set_param('HL2_ex2b', 'PreLoadFcn', num2str(Fs));
+%%
+H_2b = (abs(fft(out.output.Data)./(fft(out.input.Data))));
+
+figure()
+plot(f,H_2b);
+xlim([0,Fs/2]);
+title("3 branch");

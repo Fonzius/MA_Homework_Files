dca0705d7f32d9b5ccf0e1a63fed00684936297e
Fonzius <posta.marcus99@gmail.com>
2022-11-15 11:34:50 +0100

    Reworked Simulink file

--- HL2/HL2.m
+++ HL2/HL2.m
@@ -15,55 +15,59 @@ R = rho*c/S;
 L = rho*l/S;
 C = V/(rho*c^2);
 
-set_param('HL2_ex1', 'PreLoadFcn', num2str(Fs));
-set_param('HL2_ex1/Resonator/R', 'R', num2str(R));
-set_param('HL2_ex1/Resonator/C', 'C', num2str(C));
-set_param('HL2_ex1/Resonator/L', 'L', num2str(L));
+%% Setup all the system R,L,C
+subsystems = find_system('HL2','SearchDepth',2,'BlockType','SubSystem');
 
-out = sim('HL2_ex1', dur);
+for ii = 1:length(subsystems)
+    if contains(subsystems(ii), 'Resonator')
+        set_param(char(append(subsystems(ii),'/R')), 'R', num2str(R));
+        set_param(char(append(subsystems(ii),'/C')), 'C', num2str(C));
+        set_param(char(append(subsystems(ii),'/L')), 'L', num2str(L));
+    end
+end
+
+set_param('HL2', 'PreLoadFcn', num2str(Fs));
+
+% Run the simulation before going on with the next parts of the code
 
 %% Ex 1.b
-f = 0:Fs/length(out.impulse.Data):Fs-(1/length(out.impulse.Data));
-H = (abs(fft(out.impulse_response.Data)./(fft(out.impulse.Data))));
- 
-figure()
-plot(f,H);
-xlim([0,Fs/2]);
+f = 0:Fs/length(out.in1x1.Data):Fs-(1/length(out.in1x1.Data));
+H = (abs(fft(out.out1x1.Data)./(fft(out.in1x1.Data))));
 
 f_0 = c/(2*pi) * sqrt(S/(V*l));
 
-%% Ex 2.a
-
-subsystems = find_system('HL2_ex2a','SearchDepth',2,'BlockType','SubSystem');
-
-for ii = 1:length(subsystems)
-    if contains(subsystems(ii), 'Resonator')
-        set_param(char(append(subsystems(ii),'/R')), 'R', num2str(R));
-        set_param(char(append(subsystems(ii),'/C')), 'C', num2str(C));
-        set_param(char(append(subsystems(ii),'/L')), 'L', num2str(L));
+maxh = 0;
+maxi = 0;
+for ii = 1:length(H)
+    if H(ii)>maxh
+        maxh = H(ii);
+        maxi = ii;
     end
 end
-set_param('HL2_ex2a', 'PreLoadFcn', num2str(Fs));
-%%
-H_2a = (abs(fft(out.output.Data)./(fft(out.input.Data))));
 
 figure()
-plot(f,H_2a);
+plot(f,H,'LineWidth',2);
 xlim([0,Fs/2]);
-title("2 branch");
-%% Ex 2.b
-subsystems = find_system('HL2_ex2b','SearchDepth',2,'BlockType','SubSystem');
+xline(f_0, 'b', 'LineWidth',2);
+xline(f(maxi), '--r', 'LineWidth',2);
+ylabel("Admittance [S]");
+xlabel("f [Hz]");
+title("Frequency Response of the Electric-Equivalent of an Helmholtz Resonator")
 
-for ii = 1:length(subsystems)
-    if contains(subsystems(ii), 'Resonator')
-        set_param(char(append(subsystems(ii),'/R')), 'R', num2str(R));
-        set_param(char(append(subsystems(ii),'/C')), 'C', num2str(C));
-        set_param(char(append(subsystems(ii),'/L')), 'L', num2str(L));
-    end
-end
-set_param('HL2_ex2b', 'PreLoadFcn', num2str(Fs));
-%%
-H_2b = (abs(fft(out.output.Data)./(fft(out.input.Data))));
+error = (f(maxi)-f_0)/100;
+
+%% Ex 2.a
+
+H_2 = (abs(fft(out.out2x2.Data)./(fft(out.in2x2.Data))));
+
+figure()
+plot(f,H_2);
+xlim([0,Fs/2]);
+title("2x2");
+
+%% Ex 2.b
+
+H_2b = (abs(fft(out.out3x3.Data)./(fft(out.in3x3.Data))));
 
 figure()
 plot(f,H_2b);

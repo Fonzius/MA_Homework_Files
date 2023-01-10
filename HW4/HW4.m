aa4ff9e190c5b9608e23d9e16c2058160fee5d76
Fonzius <posta.marcus99@gmail.com>
2023-01-10 12:05:34 +0100

    Updates

--- HW4/HW4.m
+++ HW4/HW4.m
@@ -3,17 +3,14 @@ clear all;
 close all;
 
 %% Point b
+close all;
+
 L = 1;
 H = 1.4;
 
-%PlateZ: old results, very high impedance, PlateZ2: new results, good
-%impedance, bad results
+ccc = [0 0.4470 0.7410]; %plot color
 
-% Z = importdata("PlateZ.txt");
-% 
-% for ii=1:3 % Removing empty rows
-%     Z(1,:) = [];
-% end
+f = [349.23, 440, 523.25, 659.25, 783.99];
 
 Z = importdata("PlateZ22.txt");
 
@@ -43,79 +40,92 @@ Z_f5 = str2double(sortrows(Z_sorted(141:175,:),4));
 % hand by choosing the individual points where to put the strings
 
 figure
-subplot(3,2,1)
+subplot(5,1,1)
 plot(Z_f1(1:10,1),Z_f1(1:10,2), '-o', Color='r');
 hold on
 plot(L-Z_f1(1:10,1),Z_f1(1:10,2), '-o', Color='r');
+hold on
+plot(Z_f1(1:10,1),H-Z_f1(1:10,2), '-o', Color='r');
+hold on
+plot(L-Z_f1(1:10,1),H-Z_f1(1:10,2), '-o', Color='r');
+title(f(1) + " [Hz]");
+xlabel("x[m]");
+ylabel("y[m]");
 
-subplot(3,2,2);
+subplot(5,1,2);
 plot(Z_f2(1:10,1),Z_f2(1:10,2), '-o', Color='g');
 hold on
 plot(L-Z_f2(1:10,1),Z_f2(1:10,2), '-o', Color='g');
+hold on
+plot(Z_f2(1:10,1),H-Z_f2(1:10,2), '-o', Color='g');
+hold on
+plot(L-Z_f2(1:10,1),H-Z_f2(1:10,2), '-o', Color='g');
+title(f(2) + " [Hz]");
+xlabel("x[m]");
+ylabel("y[m]");
 
-subplot(3,2,3);
+subplot(5,1,3);
 plot(Z_f3(1:10,1),Z_f3(1:10,2), '-o', Color='b');
 hold on
 plot(L-Z_f3(1:10,1),Z_f3(1:10,2), '-o', Color='b');
+hold on
+plot(Z_f3(1:10,1),H-Z_f3(1:10,2), '-o', Color='b');
+hold on
+plot(L-Z_f3(1:10,1),H-Z_f3(1:10,2), '-o', Color='b');
+title(f(3) + " [Hz]");
+xlabel("x[m]");
+ylabel("y[m]");
 
-subplot(3,2,4);
+subplot(5,1,4);
 plot(Z_f4(1:10,1),Z_f4(1:10,2), '-o', Color='k');
 hold on
 plot(L-Z_f4(1:10,1),Z_f4(1:10,2), '-o', Color='k');
+hold on
+plot(Z_f4(1:10,1),H-Z_f4(1:10,2), '-o', Color='k');
+hold on
+plot(L-Z_f4(1:10,1),H-Z_f4(1:10,2), '-o', Color='k');
+title(f(4) + " [Hz]");
+xlabel("x[m]");
+ylabel("y[m]");
 
-subplot(3,2,5);
-plot(Z_f5(1:10,1),Z_f5(1:10,2), '-o', Color='magenta');
+subplot(5,1,5);
+plot(Z_f5(10:20,1),Z_f5(1:10,2), '-o', Color='magenta');
 hold on
 plot(L-Z_f5(1:10,1),Z_f5(1:10,2), '-o', Color='magenta');
+hold on
+plot(Z_f5(1:10,1),H-Z_f5(1:10,2), '-o', Color='magenta');
+hold on
+plot(L-Z_f5(1:10,1),H-Z_f5(1:10,2), '-o', Color='magenta');
+title(f(5) + " [Hz]");
+xlabel("x[m]");
+ylabel("y[m]");
 
 
-
-%Chosen points for the bridge. Values are for PlateZ so the bridge for
-%PlateZ2 will look weird
-
-%For PlateZ
-% Z_final = [Z_f1(2,1), H-Z_f1(2,2), Z_f1(2,3:5); ...
-%            Z_f2(1,1), H-Z_f2(1,2), Z_f2(1,3:5); ...
-%            Z_f3(6,1), H-Z_f3(6,2), Z_f3(6,3:5); ...
-%            L-Z_f4(2,1), Z_f4(2,2), Z_f4(2,3:5); ...
-%            L-Z_f5(6,1), Z_f5(6,1), Z_f5(6,3:5); ...
-%            ];
-
-% For PlateZ2
-Z_final = [Z_f1(2,1), Z_f1(2,2), Z_f1(2,3:5); ...
-           Z_f2(1,1), Z_f2(1,2), Z_f2(1,3:5); ...
-           Z_f3(5,1), Z_f3(5,2), Z_f3(5,3:5); ...
-           L-Z_f4(3,1), Z_f4(3,2), Z_f4(3,3:5); ...
-           L-Z_f5(1,1), Z_f5(1,2), Z_f5(1,3:5); ...
+Z_final = [L-Z_f1(7,1), H-Z_f1(7,2), Z_f1(7,3:5); ...
+           L-Z_f2(4,1), H-Z_f2(4,2), Z_f2(4,3:5); ...
+           L-Z_f3(10,1), Z_f3(10,2), Z_f3(10,3:5); ...
+           L-Z_f4(4,1), Z_f4(4,2), Z_f4(4,3:5); ...
+           Z_f5(7,1), Z_f5(7,2), Z_f5(7,3:5); ...
            ];
 figure
-plot(Z_final(:,1), Z_final(:,2));
+plot(Z_final(:,1), Z_final(:,2), '-o', LineWidth=2);
 axis([0,L, 0,H]);
-title("Bridge Design");
+xlabel("x[m]");
+ylabel("y[m]");
+%title("Bridge Design");
 
 %% Point c
+
 rho = 10.8e-3; %[kg/m] piano wire linear density, from the HW text
 
 f = [349.23, 440, 523.25, 659.25, 783.99];
 omega = f.*2.*pi;
 
-c = 2.*Z_final(:,2).*f'; %lambda * f
-%T = rho.*c.^2;
-T = [1000;1000;1000;1000;1000];
-
-%c = sqrt(T/rho);
-
-%Z0 = T./c;
+T = [800;800;800;800;800];
 Z0 = sqrt(T.*rho);
-
 Y = 1./(Z_final(:,5));
 
 % Acoustics of musical instruments, page 275, eq 6.34 (also on Antonacci's slides)
-% This is the source of all the problems probably. It's way too small for
-% PlateZ and since real(X) is what determines the damping the string will
-% basically not dampen. For PlateZ2 for some reason its values will break
-% everything that comes after this
-
 
 X = (Y .* 1j .* Z0 .* omega')./pi;
 
@@ -123,8 +133,7 @@ a = zeros(length(f), 300);
 b = zeros(length(f), 300);
 epsilon = zeros(length(f), 300);
 
-% Detune of the string. Built so that omega*epsilon always goes from -3 to 3 
-% as in the graphs
+
 for ii = 1:length(f)
     epsilon(ii,:) = linspace(-0.02*(omega(ii)), 0.02*(omega(ii)), 300);
 end
@@ -137,84 +146,141 @@ for ii = 1:length(f)
     
 end
 
-
-%Plot of eigenfrequency shift wrt string detune. Should be like graphs at
-%page 279. For values in PlateZ the graph looks convincing, for PlateZ2 not
-%as much: either a or b is way too big or way too small.
-
 figure
 for ii = 1:length(f)
     subplot(length(f),1,ii);
-    plot(epsilon(ii,:), real(a(ii,:)));
+    plot(epsilon(ii,:), real(a(ii,:)), Color=ccc);
     hold on
-    plot(epsilon(ii,:), real(b(ii,:)));
-    title(ii + "th freq " + f(ii));
-    xlabel("Epsilon [Hz]");
-    ylabel("Eigenmode Shift[Hz]");
-    legend("a","b");
+    plot(epsilon(ii,:), real(b(ii,:)), Color=ccc);
+    title(f(ii) + " [Hz]");
+    xlabel("$\epsilon$ [Hz]", Interpreter="latex");
+    ylabel("$\Re{[a]}$[Hz]", Interpreter="latex");
+    xlim([epsilon(ii,1) epsilon(ii,300)]);
+    %legend("a","b");
     grid minor
 end
 
 figure
 for ii = 1:length(f)
     subplot(length(f),1,ii);
-    plot(epsilon(ii,:), imag(a(ii,:)));
+    plot(epsilon(ii,:), imag(a(ii,:)), Color=ccc);
     hold on
-    plot(epsilon(ii,:), imag(b(ii,:)));
-    title(ii + "th freq " + f(ii));
-    xlabel("Epsilon [Hz]");
-    ylabel("Time Decay");
-    legend("a","b");
+    plot(epsilon(ii,:), imag(b(ii,:)), Color=ccc);
+    title(f(ii) + " [Hz]");
+    xlabel("$\epsilon$ [Hz]", Interpreter="latex");
+    ylabel("$\Im{[a]}$[$s^{-1}$]", Interpreter="latex");
+    xlim([epsilon(ii,1) epsilon(ii,300)]);
+    %legend("a","b");
     grid minor
 end
 
 %% Point d
+close all;
 
-t = linspace(0,1,1000); %time axis
+t_int = 60000;
 
-%Displacement computed for an arbitrary epsilon. The higher/lower epsilon
-%should give more minima, epsilon values around the middle (150th index)
-%should give less minima. For PlateZ if you increase by multiple orders of
-%magnitude X the reults are very convincing, for PlateZ2 it seems that the
-%damping is way too strong or something
+t = linspace(0,80,t_int); %time axis
 
+%eps_index = [150, 200, 250];
 
-eps_index = 100;
+R = zeros(5,t_int,3);
 
-F0 = 1; % F(0), initial condition for force on the strings
-mu = sqrt(epsilon(:,eps_index).^2 + (X.^2)); 
-
-X_n = X./(omega');
-
-
-% paper, eq 19
-param_p = X+mu;
-param_m = X-mu;
-
-R = abs((param_p.*exp(1j.*param_p.*t) - param_m.*exp(1j.*param_m.*t))./(2.*mu)).^2;
-
-for ii = 1:length(f)
-    figure
-    plot(t,10*log10(R(ii,:)));
-    title(f(ii));
+for ii = 1:300
+    mu = sqrt(epsilon(:,ii).^2 + (X.^2)); 
+    
+    X_n = X./(omega');
+    
+    % paper, eq 19
+    param_p = X+mu;
+    param_m = X-mu;
+    
+    R(:,:,ii) = abs((param_p.*exp(1j.*param_p.*t) - param_m.*exp(1j.*param_m.*t))./(2.*mu)).^2;
 end
 
-%%
+figure
+position = 1;
+
+for ii = 1:5
+    for jj = 150:50:300
+        subplot(5,4,position)
+        plot(t,10*log10(R(ii,:,jj)));
+        title(f(ii) + " [Hz], $\epsilon$ = " + epsilon(ii, jj) + " [Hz]", Interpreter="latex");
+        xlim([0 1]);
+
+        position = position+1;
+    end
+end
+
+T60 = zeros(size(epsilon));
+
+for ii = 1:5
+    for jj = 1:300
+
+        R_db = 10*log10(R(ii,:,jj)./R(ii,1,jj));
+        found = false;
+        kk = 1;
+        while found == false && kk <= t_int
+            if R_db(kk) <= -60
+                T60(ii,jj) = t(kk);
+                found = true;
+            end
+            kk = kk+1;
+        end
+    end
+end
+
+figure
+for ii = 1:5
+    subplot(5,1,ii)
+    plot(epsilon(ii,:), T60(ii,:));
+    xlim([epsilon(ii,1) epsilon(ii,300)]);
+    title(f(ii) + " [Hz]");
+    grid minor;
+end
+
+
+%% TIME CONSTANTS
+close all;
 
 tau_a = 1./imag(a);
 tau_b = 1./imag(b);
-T60 = zeros(length(f),1);
-for ii = 1:length(f)
 
-    R_db = 10*log10(R(ii,:)./R(ii,1));
-    found = false;
-    jj = 1;
-    while found == false
-        if R_db(jj) <= -60
-            T60(ii) = t(jj);
-            found = true;
-        end
-        jj = jj+1;
-    end
+figure
+subplot(2,1,1)
+semilogy(epsilon,tau_a);
+xlabel("$\epsilon$ [Hz]", Interpreter="latex");
+ylabel("$\tau_+$[s]]", Interpreter="latex");
+subplot(2,1,2)
+semilogy(epsilon, tau_b);
+xlabel("$\epsilon$ [Hz]", Interpreter="latex");
+ylabel("$\tau_-$[s]", Interpreter="latex");
 
+%% FULL FREQUENCY PLOTS
+close all;
+
+frm = importdata("FRM.txt");
+
+for ii=1:3 % Removing empty rows
+    frm(1,:) = [];
 end
+
+%frm_sorted = zeros(35,251);
+frm_f = str2double(frm(1:251,3));       %full frequency axis
+
+points = frm(1:251:8785, 1:2);                        %list of points
+frm_sorted = reshape(str2double(frm(:,4)),[251,35])'; %each row has the frf at one point
+
+figure
+for ii = 1:length(points(:,1))
+    subplot(5,7,ii);
+    plot(frm_f,frm_sorted(ii,:));
+    xlim([frm_f(1) frm_f(251)]);
+    ylim([30,80]);
+    xlabel("x: " + points(ii,1) + " y: " + points(ii,2));
+    
+end
+
+
+
+
+
--- HW5/HL4.m
+++ HW5/HL4.m
@@ -4,12 +4,12 @@ clc;
 %%
 close all;
 
-L = 1.33;
+L = 1.37;
 r = 0.6e-2;
-c = 343;
+c = 343.2816;
 f = linspace(50,1200,256);
-k = 2.*pi.*f/c;
-rho = 1.225;
+k = 2*pi*f/c;
+rho = 1.2039;
 
 
 Z_comsol = importdata("tube.txt");
@@ -27,13 +27,17 @@ end
 
 Z_antonacci = str2double(Z_antonacci);
 
-Zl = (0.25.*(f.*2.*pi).^2.*rho./pi./c+1j.*0.61.*rho.*f*2*pi/pi./r);
-Z = ZIN1(r,r,L,Zl,k,rho,c);
+Z0 = (rho*c/(pi*r^2));
+
+Z_L = (0.25.*(f.*2.*pi).^2.*rho./pi./c+1j.*0.61.*rho.*f*2*pi/pi./r);
+Z = Z0.* (Zl.*cos(k.*L) + 1j.*Z0.*sin(k.*L))./(1j.*Zl.*sin(k.*L) + Z0.*cos(k.*L));
+
 
 figure
 plot(f,db(Z));
 hold on
-plot(f,Z_comsol(:,2)');
-hold on
-plot(f,Z_antonacci(:,2)','--');
-legend("Matlab", "Ours", "Antonacci");
+plot(f,Z_comsol(:,2)', '--');
+legend("Matlab Results", "Comsol Simulation");
+xlabel("Frequency[Hz]", 'Interpreter','latex');
+ylabel("Impedance dB [$\frac{Kg*s}{m^4}$]", 'Interpreter','latex');
+grid("minor");
